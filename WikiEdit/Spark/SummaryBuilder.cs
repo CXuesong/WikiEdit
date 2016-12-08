59de1477e0bbf352886b03e3810513abe356bddf
forest93 <forest93@163.com>
2016-12-08 20:56:51 +0800

    Enhanced automatic summary generator with WikiDiffSummary.

--- WikiEdit/Spark/SummaryBuilder.cs
+++ WikiEdit/Spark/SummaryBuilder.cs
@@ -9,6 +9,7 @@ using DiffMatchPatch;
 using MwParserFromScratch;
 using MwParserFromScratch.Nodes;
 using Unclassified.TxLib;
+using WikiDiffSummary;
 
 namespace WikiEdit.Spark
 {
@@ -17,116 +18,147 @@ namespace WikiEdit.Spark
     /// </summary>
     public static class SummaryBuilder
     {
-        private class WikitextSection
-        {
-            public Heading Heading { get; set; }
-
-            public WikitextSection Parent { get; set; }
-
-            public string Content { get; set; }
-
-            public int Start { get; set; }
-
-            public int Length { get; set; }
-
-            public int End => Start + Length;
-
-            public int InsertedChars { get; set; }
-
-            public int DeletedChars { get; set; }
-        }
-
         /// <summary>
         /// Generates summary by comparing the contents of wikitext.
         /// </summary>
         /// <returns>The auto-generated summary text.</returns>
         public static string BuildWikitextSummary(string text1, string text2)
         {
-            var parser = new WikitextParser();
-            var wt1 = parser.Parse(text1);
-            var wt2 = parser.Parse(text2);
-            var sections1 = WikitextSplitSections(text1, wt1);
-            var dmp = new diff_match_patch();
-            var diffs = dmp.diff_main(text1, text2);
-            // We don't want too many segments stuck together, which may lead into
-            // inaccurate results.
-            dmp.diff_cleanupSemanticLossless(diffs);
-            var position1 = 0;
-            var sectionId1 = 0;
-            // Check the number of characters changed in each section.
-            foreach (var diff in diffs)
+            var cmp = new WikitextBySectionComparer();
+            var diff = cmp.Compare(text1 ?? "", text2 ?? "");
+            var headingCounter = new Dictionary<string, int> {{"top", 1}};
+            var sb = new StringBuilder();
+            Action<StringBuilder, SectionDiff> buildLengthChange = (b, d) =>
             {
-                while (position1 > sections1[sectionId1].End)
-                    sectionId1++;
-                var section = sections1[sectionId1];
-                switch (diff.operation)
+                // Print length change
+                if (d.AddedChars > 0 || d.RemovedChars > 0)
                 {
-                    case Operation.DELETE:
-                        section.DeletedChars += diff.text.Length;
-                        position1 += diff.text.Length;
-                        break;
-                    case Operation.INSERT:
-                        section.InsertedChars += diff.text.Length;
-                        break;
-                    case Operation.EQUAL:
-                        position1 += diff.text.Length;
+                    b.Append('(');
+                    if (d.AddedChars > 0)
+                    {
+                        b.Append('+');
+                        b.Append(d.AddedChars);
+                        if (d.RemovedChars > 0)
+                            b.Append(' ');
+                    }
+                    if (d.RemovedChars > 0)
+                    {
+                        b.Append('-');
+                        b.Append(d.RemovedChars);
+                    }
+                    b.Append(')');
+                }
+            };
+            foreach (var d in diff)
+            {
+                if (d.Status == SectionDiffStatus.Identical)
+                {
+                }
+                else if (d.Section1 != null)
+                {
+                    Debug.Assert(d.Status == SectionDiffStatus.Removed
+                                 || d.Status == SectionDiffStatus.Modified
+                                 || d.Status == SectionDiffStatus.WhitespaceModified);
+                    // Print original heading path
+                    if (d.Section2?.Path == d.Section1.Path)
+                    {
+                        sb.Append(BuildSectionLink(d.Section1.Path, null,
+                            headingCounter.TryGetValue(d.Section1.Path.LastOrDefault() ?? "top"),
+                            true));
+                    }
+                    else
+                    {
+                        sb.Append(d.Section1.Path);
+                        sb.Append(':');
+                    }
+                    buildLengthChange(sb, d);
+                    // Print summary
+                    switch (d.Status)
+                    {
+                        case SectionDiffStatus.Removed:
+                            sb.Append(Tx.T("editor.summary builder.removed section"));
+                            break;
+                        case SectionDiffStatus.Modified:
+                        case SectionDiffStatus.WhitespaceModified:
+                            Debug.Assert(d.Section2 != null);
+                            if (d.Section1.Path != d.Section2.Path)
+                            {
+                                // Section rename
+                                var newPathExpr = BuildSectionLink(d.Section2.Path, d.Section1.Path,
+                                    headingCounter.TryGetValue(d.Section2.Path.LastOrDefault() ?? "top"),
+                                    false);
+                                sb.Append(Tx.T("editor.summary builder.renamed section", "path", newPathExpr));
+                            }
+                            if (d.Status == SectionDiffStatus.WhitespaceModified)
+                                sb.Append(Tx.T("editor.summary builder.whitespace changes"));
+                            break;
+                    }
+                    sb.Append(' ');
+                }
+                else
+                {
+                    Debug.Assert(d.Section2 != null);
+                    Debug.Assert(d.Status == SectionDiffStatus.Added);
+                    sb.Append(BuildSectionLink(d.Section2.Path, null,
+                        headingCounter.TryGetValue(d.Section2.Path.LastOrDefault() ?? "top"),
+                        true));
+                    buildLengthChange(sb, d);
+                    sb.Append(Tx.T("editor.summary builder.new section"));
+                    sb.Append(' ');
+                }
+                if (d.Section2 != null && d.Section2.Path.Length > 0)
+                {
+                    var heading = d.Section2.Path.Last();
+                    headingCounter[heading] = headingCounter.TryGetValue(heading) + 1;
+                }
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Build a section anchor link for edit summary.
+        /// </summary>
+        /// <param name="sectionPath">The section path.</param>
+        /// <param name="originalSectionPath">The section path before modification. This is used to trace the renamed heading.</param>
+        /// <param name="serial">The serial number of the section, if there're more than one sections with the same name.</param>
+        /// <returns>The desired wikitext.</returns>
+        private static string BuildSectionLink(SectionPath sectionPath, SectionPath originalSectionPath
+            , int serial, bool withColon)
+        {
+            Debug.Assert(sectionPath != null);
+            if (sectionPath.Length == 0) return "/*top*/";
+            if (sectionPath.Length == 1 && !sectionPath[0].Contains("*/"))
+            {
+                var title = sectionPath[0];
+                if (serial > 0) title += "_" + serial;
+                return "/*" + title + "*/";
+            }
+            // Here we do not use MediaWiki's built-in anchor expression, i.e. /* name */
+            // because it cannot show sufficient information to locate a section heading.
+            // Instead, we use HeadingA/HeadingAA/[[#HeadingAAA]] to represent the full path.
+            var sb = new StringBuilder();
+            int i = 0;
+            if (originalSectionPath != null)
+            {
+                // Skip the common segments.
+                for (; i < sectionPath.Length - 1; i++)
+                {
+                    if (i >= originalSectionPath.Length || sectionPath[i] != originalSectionPath[i])
                         break;
                 }
             }
-            // Then summarize them.
-            var summary = string.Join(" ", sections1.Where(s => s.InsertedChars + s.DeletedChars > 0)
-                .Select(s =>
-                {
-                    var headingName = s.Heading == null ? "top" : string.Join(null, s.Heading.Inlines).Trim();
-                    var segment = "/*" + headingName + "*/";
-                    string change = null;
-                    if (s.InsertedChars > 0) change = "+" + s.InsertedChars;
-                    if (s.DeletedChars > 0) change = (change == null ? null : change + " ") + "-" + s.DeletedChars;
-                    return segment + Tx.P(change);
-                }));
-            return summary;
-        }
-
-        private static WikitextSection SectionFromPosition(IList<WikitextSection> sections, int position)
-        {
-            foreach (var sect in sections)
+            if (i > 0) sb.Append(".../");
+            for (; i < sectionPath.Length - 1; i++)
             {
-                if (sect.Start <= position && sect.Start + sect.Length > position) return sect;
-            }
-            return null;
-        }
 
-        private static IList<WikitextSection> WikitextSplitSections(string text, Wikitext wt)
-        {
-            Debug.Assert(text != null);
-            Debug.Assert(wt != null);
-            Debug.Assert(text == wt.ToString());
-            var result = new List<WikitextSection>();
-            var levelStack = new Stack<WikitextSection>();
-            var pos = 0;
-            Heading lastHeading = null;
-            foreach (var h in wt.EnumDescendants().OfType<Heading>())
-            {
-                // Split the text
-                var span = (IWikitextSpanInfo) h;
-                var item = new WikitextSection
-                {
-                    Heading = lastHeading,
-                    Content = text.Substring(pos, span.Start - pos),
-                    Start = pos,
-                    Length = span.Start - pos
-                };
-                result.Add(item);
-                // Determine the level
-                while (levelStack.Count > 0 && levelStack.Peek().Heading.Level > h.Level)
-                    levelStack.Pop();
-                if (levelStack.Count > 0)
-                    item.Parent = levelStack.Peek();
-//
-                pos = span.Start + span.Length;
-                lastHeading = h;
+                sb.Append(sectionPath[i]);
+                sb.Append('/');
             }
-            return result;
+            sb.Append("[[#");
+            sb.Append(sectionPath.Last());
+            sb.Append("]]");
+            if (withColon) sb.Append(":");
+            return sb.ToString();
         }
     }
 }
